c1ad6c3a75fc078909d7a60d13534c872afde78c
Kiswari10 <kiswari10@gmail.com>
2020-07-24 02:39:36 -0500

    add url in environment

--- src/app/services/unsplash.service.ts
+++ src/app/services/unsplash.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UnsplashService {
+  url = environment.url;
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
+
+  getPhotos() {
+    return this.http.get(`${this.url}photos?page=1&per_page=20&client_id=tOoD8N49YdY2HrKYAlMcWLbam4IeRJeD2-VL4TdGUD4`);
+  }
 }
